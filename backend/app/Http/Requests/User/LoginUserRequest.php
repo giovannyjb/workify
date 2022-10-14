f261161ec971ef029600aa948d503b9656b9b78b
giovannyjb <giiovaanny11@gmail.com>
2022-10-13 19:15:06 -0500

    first commit

---
+++ backend/app/Http/Requests/User/LoginUserRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\ValidationException;
+
+class LoginUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            "email" => "required|string",
+            "password" => "required|string"
+            //
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $aux = (new ValidationException($validator))->errors();
+
+        throw new HttpResponseException(response()->json(["errors" => $aux, "status" => "error"]));
+    }
+}
